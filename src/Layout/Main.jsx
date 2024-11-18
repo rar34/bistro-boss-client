86889dd809ac44d378554a26483b286ac98008c8
rar34 <ra.cse34@gmail.com>
2024-11-18 23:34:41 +0600

    added footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer";
 
 const Main = () => {
     return (
         <div>
+            <div className="min-h-[calc(100vh-250px)]">
             <Outlet />
+            </div>
+            <Footer />
         </div>
     );
 };
