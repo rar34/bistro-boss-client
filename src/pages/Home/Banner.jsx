ff47f9f7882668fffc3425f7a3e7cb4a96c73c81
rar34 <ra.cse34@gmail.com>
2024-11-22 21:04:00 +0600

    add all image in banner

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,33 +1,40 @@
-import React from 'react';
-var ReactDOM = require('react-dom');
-var Carousel = require('react-responsive-carousel').Carousel;
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import img1 from '../../assets/home/01.jpg'
+import img2 from '../../assets/home/02.jpg'
+import img3 from '../../assets/home/03.png'
+import img4 from '../../assets/home/04.jpg'
+import img5 from '../../assets/home/05.png'
+import img6 from '../../assets/home/06.png'
+
 const Banner = () => {
     return (
-        <Carousel showArrows={true} onChange={onChange} onClickItem={onClickItem} onClickThumb={onClickThumb}>
+        <Carousel>
                 <div>
-                    <img src="assets/home/01.jpg" />
-                    <p className="legend">Legend 1</p>
+                    <img src={img1} />
+                    
                 </div>
                 <div>
-                    <img src="assets/2.jpeg" />
-                    <p className="legend">Legend 2</p>
+                    <img src={img2} />
+                    
                 </div>
                 <div>
-                    <img src="assets/3.jpeg" />
-                    <p className="legend">Legend 3</p>
+                    <img src={img3} />
+                    
                 </div>
                 <div>
-                    <img src="assets/4.jpeg" />
-                    <p className="legend">Legend 4</p>
+                    <img src={img4} />
+                    
                 </div>
                 <div>
-                    <img src="assets/5.jpeg" />
-                    <p className="legend">Legend 5</p>
+                    <img src={img5} />
+                    
                 </div>
                 <div>
-                    <img src="assets/6.jpeg" />
-                    <p className="legend">Legend 6</p>
+                    <img src={img6} />
+                    
                 </div>
+                
             </Carousel>
     );
 };
