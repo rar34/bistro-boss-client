7614142e22f743e8e272bdfc7464fafaa040603a
rar34 <ra.cse34@gmail.com>
2024-11-24 23:23:07 +0600

    create dynamic section title components

--- src/pages/Home/Category.jsx
+++ src/pages/Home/Category.jsx
@@ -13,11 +13,13 @@ import slide2 from '../../assets/home/slide2.jpg'
 import slide3 from '../../assets/home/slide3.jpg'
 import slide4 from '../../assets/home/slide4.jpg'
 import slide5 from '../../assets/home/slide5.jpg'
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
 
 
 const Category = () => {
     return (
         <div className='my-14'>
+            <SectionTitle heading={"order online"} subHeading={"From 11am to 10pm"}></SectionTitle>
             <Swiper
                 slidesPerView={4}
                 spaceBetween={30}
