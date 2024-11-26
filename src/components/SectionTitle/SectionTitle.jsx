3bbd1bb9c847e6203f301e44260bf7be289a1aba
rar34 <ra.cse34@gmail.com>
2024-11-26 22:32:46 +0600

    change font

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@ const SectionTitle = ({heading, subHeading}) => {
     return (
         <div className="text-center mx-auto md:w-4/12">
             <p className="text-yellow-600 mb-2 italic">---{subHeading}---</p>
-            <h3 className="text-4xl border-y-4 uppercase mb-14 py-4">{heading}</h3>
+            <h3 className="text-4xl border-y-4 uppercase font-fontCinzen mb-14 py-4">{heading}</h3>
         </div>
     );
 };
--- src/pages/Home/BBoss.jsx
+++ src/pages/Home/BBoss.jsx
@@ -11,7 +11,7 @@ const BBoss = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-neutral-content p-10 md:p-16 text-center">
                 <div className="bg-white text-black p-10">
-                    <h1 className="mb-5 text-5xl uppercase">Bistro Boss</h1>
+                    <h1 className="mb-5 text-5xl font-fontCinzen uppercase"><span className='text-6xl'>B</span>istro <span className='text-6xl'>B</span>oss</h1>
                     <p className="mb-5">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.
                     </p>
