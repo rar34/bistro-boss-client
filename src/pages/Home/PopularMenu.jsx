74e50456739635f9e9d578a08addd1bb65e90d4b
rar34 <ra.cse34@gmail.com>
2024-12-10 23:17:52 +0600

    added view full menu button in popular menu section and link the menu page

--- src/pages/Home/PopularMenu.jsx
+++ src/pages/Home/PopularMenu.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 import MenuItem from "../Shared/MenuItem";
+import { Link } from "react-router-dom";
 
 const PopularMenu = () => {
 
@@ -18,8 +19,11 @@ const PopularMenu = () => {
             <SectionTitle heading={'FROM OUR MENU'} subHeading={"Check it out"} />
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-16 max-w-screen-xl mx-auto">
                 {
-                    menu.filter(i=>i.category === 'popular').map(item => <MenuItem key={item._id} image={item.image} name={item.name} recipe={item.recipe} price={item.price}></MenuItem>)
+                    menu.filter(i => i.category === 'popular').map(item => <MenuItem key={item._id} image={item.image} name={item.name} recipe={item.recipe} price={item.price}></MenuItem>)
                 }
+            </div>
+            <div className="text-center">
+                <Link to={"/menu"}><button className="text-xl font-medium border-b-4 rounded-lg  uppercase border-black py-4 px-7">View Full  Menu</button></Link>
             </div>
         </div>
     );
