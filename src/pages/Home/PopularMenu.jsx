0bd1d13266fdc6dc4f23793c729ad0e471bebc04
rar34 <ra.cse34@gmail.com>
2024-12-12 18:36:32 +0600

    filter popular menu from menu

--- src/pages/Home/PopularMenu.jsx
+++ src/pages/Home/PopularMenu.jsx
@@ -10,7 +10,10 @@ const PopularMenu = () => {
     useEffect(() => {
         fetch('menu.json')
             .then(res => res.json())
-            .then(data => setMenu(data))
+            .then(data => {
+                const popularItems = data.filter(item=> item.category === 'popular')
+                setMenu(popularItems)
+            })
     }, [])
 
     console.log(menu)
@@ -19,7 +22,7 @@ const PopularMenu = () => {
             <SectionTitle heading={'FROM OUR MENU'} subHeading={"Check it out"} />
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-16 max-w-screen-xl mx-auto">
                 {
-                    menu.filter(i => i.category === 'popular').map(item => <MenuItem key={item._id} image={item.image} name={item.name} recipe={item.recipe} price={item.price}></MenuItem>)
+                    menu.map(item => <MenuItem key={item._id} image={item.image} name={item.name} recipe={item.recipe} price={item.price}></MenuItem>)
                 }
             </div>
             <div className="text-center">
