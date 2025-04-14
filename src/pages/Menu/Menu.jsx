7929ea16164acc78bb06bb216724a527cfbedb40
rar34 <ra.cse34@gmail.com>
2025-04-14 23:19:06 +0600

    make cover as a dynamic component

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../Shared/Cover";
+import menuImg from '../../assets/menu/banner3.jpg'
 
 const Menu = () => {
     return (
@@ -7,7 +8,7 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <Cover></Cover>
+            <Cover img={menuImg} title={"our menu"} subTitle={"would to like to try a dish ?"}></Cover>
         </div>
     );
 };
